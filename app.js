3763e2d75e7ae0e0304190542b44d544cda599b6
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-04-01 23:46:40 +0900

    ルーティングまで対応

--- app.js
+++ app.js
@@ -2,7 +2,14 @@
 // httpモジュールを読み込んでインスタンス化する。
 const http = require('http');
 const fs = require('fs');
+const ejs = require('ejs');
+// urlモジュールを読み込んでインスタンス化する。
+const url = require('url');
 
+// ejsファイルの読み込み
+const index_page = fs.readFileSync('./index.ejs', 'utf8');
+// cssファイルの読み込み
+const test_css = fs.readFileSync('./test.css', 'utf8');
 // サーバーオブジェクトを構築する。
 var server = http.createServer (getFromClient);
 // ポート3000で展開する。(待ち受け状態にする。)
@@ -12,12 +19,28 @@ console.log('Server start!');
 
 // サーバー構築用の関数
 function getFromClient(request, response) {
-    // htmlファイルを読み込む
-        fs.readFile('./index.html', 'UTF-8', 
-        // エラーの場合の処理
-        (error, data) => {
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.write(data);
-        response.end();
-    });
+    // URLを構築するための変数を用意
+    var url_parts = url.parse(request.url);
+    switch (url_parts.pathname) {
+        case '/':
+            // index.ejsをレンダリングする。
+            var content = ejs.render(index_page, {
+                title: "Indexページ",
+                content: "これはテンプレートを使ったサンプルページです。",
+            });
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.write(content);
+            response.end();
+            break;
+        
+        case '/test.css':
+            response.writeHead(200, { 'Content-Type': 'text/css' });
+            response.write(test_css);
+            response.end();
+            break;
+        
+        default:
+            response.writeHead(200, { 'Content-Type': 'text/plain' });
+            response.write('no pages...');
+    }
 }
