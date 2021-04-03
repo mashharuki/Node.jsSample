2e8c400972727fe4e7dfbe6f56b724ef3c07b47d
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-04-03 21:11:59 +0900

    クッキーを扱うところまでコミット

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const ejs = require('ejs');
 // urlモジュールを読み込んでインスタンス化する。
 const url = require('url');
-const qs = require('querystring')
+const qs = require('querystring');
 
 // ejsファイルの読み込み
 const index_page = fs.readFileSync('./index.ejs', 'utf8');
@@ -13,19 +13,14 @@ const other_page = fs.readFileSync('./other.ejs', 'utf8')
 // cssファイルの読み込み
 const test_css = fs.readFileSync('./test.css', 'utf8');
 // オブジェクト変数 dataを用意する。
-var data = {
-    'Taro': '09-999-999',
-    'Hanako': '080-888-999',
-    'Sachiko': '070-777-777',
-    'Ichiro': '060-666-666'
-}
+var data = { msg: 'no message...' };
 // オブジェクト変数 data2を用意する。
 var data2 = {
     'Taro': ['taro@yamada', '09-999-999', 'Tokyo'],
     'Hanako': ['hanako@flower', '080-888-999', 'Yokohama'],
     'Sachiko': ['sachi@happy', '070-777-777', 'Nagoya'],
     'Ichiro': ['ichi@baseball', '060-666-666', 'USA'],
-}
+};
 // サーバーオブジェクトを構築する。
 var server = http.createServer (getFromClient);
 // ポート3000で展開する。(待ち受け状態にする。)
@@ -61,18 +56,26 @@ function getFromClient(request, response) {
 
     // index画面のアクセス処理
     function response_index(request, response){
-        // 値をセットする。
-        var msg = "これはIndexページです。";
-        // index.ejsをレンダリングする。
-        var content = ejs.render(index_page, {
-            title: "Index",
-            content: msg,
-            data: data,
-            filename: 'data_item'
-        });
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.write(content);
-        response.end();
+        // POSTアクセス時の処理
+        if (request.method == 'POST') {
+            var body = '';
+             // データ受信のイベント処理
+            request.on ('data', (data) => {
+                body += data;
+            });
+            // データ受信完了後のイベント処理
+            request.on('end', () => {
+                // データの解析
+                data = qs.parse(body);
+                // クッキーの保存
+                setCookie('msg', data.msg, response);
+                // メソッドの呼び出し
+                write_index(request, response);
+            });
+        } else {
+            // メソッドの呼び出し
+            write_index(request, response);
+        }
     }
 
     // other画面のアクセス処理
@@ -99,5 +102,48 @@ function getFromClient(request, response) {
             response.write(content);
             response.end();
         });
+    }
+
+    // Index画面の作成表示画面
+    function write_index(request, response) {
+        // 値をセットする。
+        var msg = "※伝言を表示します。";
+        // クッキーデータを取得する。
+        var cookie_data = getCookie('msg', request);
+        // index.ejsをレンダリングする。
+        var content = ejs.render(index_page, {
+            title: "Index",
+            content: msg,
+            data: data,
+            cookie_data: cookie_data,
+        });
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write(content);
+        response.end();
+    }
+
+    // クッキーの値を設定
+    function setCookie (key, value, response) {
+        // エスケープ処理を施す
+        var cookie = escape (value);
+        // ヘッダーにクッキー情報をセット
+        response.setHeader('Set-Cookie', [key + '=' + cookie]);
+    }
+
+    // クッキーの値を取得
+    function getCookie (key, request) {
+        // クッキーデータを取得する
+        var cookie_data = request.headers.cookie != undefined ? request.headers.cookie : '';
+        // ; で分割する。
+        var data = cookie_data.split(';');
+        
+        for ( var i in data){
+            // 前後の余白を取り除く
+            if(data[i].trim().startsWith(key + '=')) {
+                var result = data[i].trim().substring(key.length + 1);
+                return unescape(result);
+            }
+        }
+        return '';
     }
 }
