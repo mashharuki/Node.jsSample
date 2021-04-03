ec8c669226640cf0718c1b537f5c9b3faaa1de78
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-04-03 17:29:30 +0900

    複数のデータを表示するパターンに修正

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ var data = {
     'Sachiko': '070-777-777',
     'Ichiro': '060-666-666'
 };
+// オブジェクト変数 data2を用意する。
+var data2 = {
+    'Taro': ['taro@yamada', '09-999-999', 'Tokyo'],
+    'Hanako': ['hanako@flower', '080-888-999', 'Yokohama'],
+    'Sachiko': ['sachi@happy', '070-777-777', 'Nagoya'],
+    'Ichiro': ['ichi@baseball', '060-666-666', 'USA'],
+};
 // サーバーオブジェクトを構築する。
 var server = http.createServer (getFromClient);
 // ポート3000で展開する。(待ち受け状態にする。)
@@ -72,35 +79,25 @@ function getFromClient(request, response) {
     function response_other(request, response) {
         var msg = "これはOtherページです。";
         // POSTアクセス時の処理
-        if (request.method == 'POST') {
-            var body = '';
-            // データ受信のイベント処理
-            request.on ('data', (data) => {
-                body += data;
-            });
-            // データ受信完了後のイベント処理
-            request.on('end', () => {
-                var post_data = qs.parse(body);
-                msg += 'あなたは、 「' + post_data.msg + '」と書きました。';
-                response_other(request, response);
-                var content = ejs.render(other_page, {
-                    title: "Other",
-                    content: msg,
-                });
-                response.writeHead(200, { 'Content-Type': 'text/html' });
-                response.write(content);
-                response.end();
-            });
-        } else {
-            var msg = "ページがありません。";
-            // 画面をレンダリングする。
+        var body = '';
+        // データ受信のイベント処理
+        request.on ('data', (data) => {
+            body += data;
+        });
+        // データ受信完了後のイベント処理
+        request.on('end', () => {
+            var post_data = qs.parse(body);
+            msg += 'あなたは、 「' + post_data.msg + '」と書きました。';
+            response_other(request, response);
             var content = ejs.render(other_page, {
                 title: "Other",
                 content: msg,
+                data: data2,
+                filename: 'data_item'
             });
             response.writeHead(200, { 'Content-Type': 'text/html' });
             response.write(content);
             response.end();
-        }    
+        });
     }
 }
